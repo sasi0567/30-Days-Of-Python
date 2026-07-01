faa18e57b4024ea381a7470630a5e95bc093a660
sasi0567 <sasivakamalla@gmail.com>
2026-07-01 16:15:39 +0530

    Create exe.py

---
+++ Day01/exe.py
@@ -0,0 +1,26 @@
+#Exercise 1
+print(2 + 6)   # addition(+)
+print(5 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Helloo'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'Name': 'Sasidhar'}))  # Dictionary
+print(type({1.2, 2.4, 3.14}))    # Tuple
+
+#Exercise 2
+#Calculating Euclidean distance
+
+x1,y1=3,4
+x2,y2=7,8
+distance=((x2-x1)**2+(y2-y1)**2)**0.5
+print(distance)
