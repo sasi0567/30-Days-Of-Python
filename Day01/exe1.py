c91e3ea60825933dfedc64d628b58163b1d87236
sasi0567 <sasivakamalla@gmail.com>
2026-07-01 15:37:23 +0530

    Create exe1.py

---
+++ Day01/exe1.py
@@ -0,0 +1,17 @@
+print(2 + 6)   # addition(+)
+print(5 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Helloo'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'Name': 'Sasidhar'}))  # Dictionary
+print(type({1.2, 2.4, 3.14}))    # Tuple
