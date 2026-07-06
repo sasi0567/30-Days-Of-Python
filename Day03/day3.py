6033828df128e2747b37f6219fab38211ed8d212
sasi0567 <sasivakamalla@gmail.com>
2026-07-06 18:11:59 +0530

    Day 3 - Operators and Comparisons

---
+++ Day03/day3.py
@@ -0,0 +1,29 @@
+#Arithmetic operators
+a=20
+b=8
+print(a+b)#Addition
+print(a-b)#Subtraction
+print(a*b)#Multiplication
+print(a**b)#Exponent
+print(a/b)#Division
+print(a//b)#Floor Division
+print(a%b)#Modulus
+#Comparison operators
+print(a==b)#Equal to 
+print(a!=b)#Not equal to
+print(a>b)#Greater than
+print(a<b)#Less than
+print(a>=b)#Greater than or equal to
+print(a<=b)#Less than or equal to
+#Logical operators
+print(a>b and b>a)#Logical AND
+print(a>b or b<a)#Logical OR
+print(not(b>a))
+#Membership operators
+print(("py" in "python"))
+print(("hi" in "python"))
+#Identity operators
+c=a
+print(a is b)
+print(a is c)
+print(a is not b)
